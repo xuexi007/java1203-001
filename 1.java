0f8cd65b97188e499f35035e2838f997963bff94
xuexi007 <44258352+xuexi007@users.noreply.github.com>
2019-05-20 11:36:57 +0800

    Update 1.java

--- 1.java
+++ 1.java
@@ -2,7 +2,7 @@
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//**********************************************************************
 @SpringBootApplication
 public class SpringBootMain {
     public static void main(String[] args) {
